fd3e0a8fb2b35047e69eb18c1cdae44bb5f3da02
Joe Chrisman <jchrisman71@gmail.com>
2022-04-26 14:13:33 -0500

    Add box around Overall Total

--- src/components/Print/PrintTotals.js
+++ src/components/Print/PrintTotals.js
@@ -2,48 +2,6 @@ import { Col, Row } from "react-bootstrap";
 import { parseNum } from "../HelperFunctions";
 
 const PrintTotals = ({ formValues, showGIKTotal = false }) => {
-    function NonGiftAmount() {
-        if (parseNum(formValues.nonGiftTotal) > 0) {
-            return (
-                <>
-                    <Col className="d-flex justify-content-center">
-                        <div
-                            className="gTlabels totals"
-                            style={{ fontWeight: "bold" }}
-                        >
-                            Non-Gift Total:
-                        </div>
-                        <div style={{ fontWeight: "bold" }}>
-                            <span
-                                className="normal"
-                                style={{ fontWeight: "bold" }}
-                            >
-                                {formValues.nonGiftTotal}
-                            </span>
-                        </div>
-                    </Col>
-
-                    <Col className="d-flex justify-content-end">
-                        <div
-                            className="gTlabels totals"
-                            style={{ fontWeight: "bold" }}
-                        >
-                            Overall Total:
-                        </div>
-                        <div>
-                            <span
-                                className="normal"
-                                style={{ fontWeight: "bold" }}
-                            >
-                                {formValues.overallTotal}
-                            </span>
-                        </div>
-                    </Col>
-                </>
-            );
-        } else return <></>;
-    }
-
     function GiftOverallTotal() {
         if (parseNum(formValues.nonGiftTotal) === 0) {
             return (
@@ -85,16 +43,54 @@ const PrintTotals = ({ formValues, showGIKTotal = false }) => {
             );
     }
 
+    function NonGiftAmount() {
+        if (parseNum(formValues.nonGiftTotal) > 0) {
+            return (
+                <>
+                    <Col className="d-flex justify-content-end">
+                        <div
+                            className="totals gTlabels"
+                            style={{ fontWeight: "bold" }}
+                        >
+                            Gift Total:
+                        </div>
+                        <div>
+                            <span className="normal">
+                                {formValues.giftTotal}
+                            </span>
+                        </div>
+                    </Col>
+                    <Col className="d-flex justify-content-center">
+                        <div
+                            className="gTlabels totals"
+                            style={{ fontWeight: "bold" }}
+                        >
+                            Non-Gift Total:
+                        </div>
+                        <div style={{ fontWeight: "bold" }}>
+                            <span
+                                className="normal"
+                                style={{ fontWeight: "bold" }}
+                            >
+                                {formValues.nonGiftTotal}
+                            </span>
+                        </div>
+                    </Col>
+                </>
+            );
+        } else return <></>;
+    }
+
     return (
         <>
             <div className="d-print-table-row mt-3 justify-content-end">
-                {/* {showGIKTotal && (
+                {showGIKTotal && (
                     <>
                         <div
                             className="totals gTlabels"
                             style={{ fontWeight: "bold" }}
                         >
-                            Gift in Kind Total!:
+                            Gift in Kind Total:
                         </div>
                         <div>
                             <span
@@ -105,19 +101,25 @@ const PrintTotals = ({ formValues, showGIKTotal = false }) => {
                             </span>
                         </div>
                     </>
-                )} */}
+                )}
 
                 {NonGiftAmount()}
-
                 <Col className="d-flex justify-content-end">
-                    <div
-                        className="totals gTlabels"
-                        style={{ fontWeight: "bold" }}
-                    >
-                        Gift Total:
-                    </div>
-                    <div>
-                        <span className="normal">{formValues.giftTotal}</span>
+                    <div className="d-flex" style={{ border: "solid 2px" }}>
+                        <div
+                            className="gTlabels totals"
+                            style={{ fontWeight: "bold" }}
+                        >
+                            Overall Total:
+                        </div>
+                        <div>
+                            <span
+                                className="normal"
+                                style={{ fontWeight: "bold" }}
+                            >
+                                {formValues.overallTotal}
+                            </span>
+                        </div>
                     </div>
                 </Col>
             </div>
