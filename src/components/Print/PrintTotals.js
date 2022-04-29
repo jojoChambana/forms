ab52e90d10a914cdca37b65bc5f0ec015cb99338
Joe Chrisman <jchrisman71@gmail.com>
2022-04-29 16:32:02 -0500

    change GIK print to add a row per Bill

--- src/components/Print/PrintTotals.js
+++ src/components/Print/PrintTotals.js
@@ -1,6 +1,45 @@
 import { parseNum } from "../HelperFunctions";
 
 const PrintTotals = ({ formType, formValues, showGIKTotal = false }) => {
+    // return (
+    //     <>
+    //         <div className="totalTable border mt-3">
+    //             <div className="total">
+    //                 Gift Total:{" "}
+    //                 <span className="normal">{formValues.giftTotal}</span>
+    //             </div>
+
+    //             {parseNum(formValues.nonGiftTotal) > 0 ? (
+    //                 <>
+    //                     <div className="total">
+    //                         Non-Gift-Total:{" "}
+    //                         <span className="normal">
+    //                             {formValues.nonGiftTotal}
+    //                         </span>
+    //                     </div>
+    //                     <div className="total">
+    //                         Total Amount:
+    //                         <span className="normal">
+    //                             {formValues.overallTotal}
+    //                         </span>
+    //                     </div>
+    //                 </>
+    //             ) : (
+    //                 <></>
+    //             )}
+
+    //             {showGIKTotal ? (
+    //                 <div className="total">
+    //                     Gift in Kind:{" "}
+    //                     <span className="normal">{formValues.gikTotal}</span>
+    //                 </div>
+    //             ) : (
+    //                 <></>
+    //             )}
+    //         </div>
+    //     </>
+    // );
+
     return (
         <>
             <div className="totalTable border mt-3">
@@ -27,16 +66,18 @@ const PrintTotals = ({ formType, formValues, showGIKTotal = false }) => {
                 ) : (
                     <></>
                 )}
+            </div>
 
-                {showGIKTotal ? (
+            {showGIKTotal ? (
+                <div className="totalTable border mt-3">
                     <div className="total">
                         Gift in Kind:{" "}
                         <span className="normal">{formValues.gikTotal}</span>
                     </div>
-                ) : (
-                    <></>
-                )}
-            </div>
+                </div>
+            ) : (
+                <></>
+            )}
         </>
     );
 };
