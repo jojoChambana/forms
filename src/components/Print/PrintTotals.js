11c52ad919cf9ae52a0b466b744a485fbc4ab27b
Joe Chrisman <jchrisman71@gmail.com>
2022-04-29 14:49:42 -0500

    fix non GIK vs GIK print totals conditional

--- src/components/Print/PrintTotals.js
+++ src/components/Print/PrintTotals.js
@@ -2,172 +2,41 @@ import { Col } from "react-bootstrap";
 import { parseNum } from "../HelperFunctions";
 
 const PrintTotals = ({ formType, formValues, showGIKTotal = false }) => {
-    function NonGiftAmount() {
-        if (parseNum(formValues.nonGiftTotal) > 0) {
-            return (
-                <>
-                    <Col className="d-flex justify-content-start">
-                        <div
-                            className="totals gTlabels vCenter"
-                            style={{
-                                fontWeight: "bold",
-                                margin: "0em 0.5em",
-                                width: "auto",
-                            }}
-                        >
-                            Gift Total:
-                        </div>
-                        <div className="vCenter">
-                            <span className="normal" style={{ padding: "0" }}>
-                                {formValues.giftTotal}
-                            </span>
-                        </div>
-                    </Col>
-                    <Col className="d-flex justify-content-center">
-                        <div
-                            className="gTlabels vCenter"
-                            style={{
-                                fontWeight: "bold",
-                                margin: "0em 0.5em",
-                                width: "auto",
-                            }}
-                        >
-                            Non-Gift Total:
-                        </div>
-                        <div className="vCenter" style={{ fontWeight: "bold" }}>
-                            <span
-                                className="normal"
-                                style={{
-                                    fontWeight: "bold",
-                                    margin: "0em 0.5em",
-                                }}
-                            >
+    return (
+        <>
+            <div className="totalTable border mt-3">
+                <div className="total">
+                    Gift Total:{" "}
+                    <span className="normal">{formValues.giftTotal}</span>
+                </div>
+
+                {parseNum(formValues.nonGiftTotal) > 0 ? (
+                    <>
+                        <div className="total">
+                            Non-Gift-Total:{" "}
+                            <span className="normal">
                                 {formValues.nonGiftTotal}
                             </span>
                         </div>
-                    </Col>
-                    <Col className="d-flex justify-content-end">
-                        <div
-                            className="gTlabels totals vCenter"
-                            style={{
-                                fontWeight: "bold",
-                                margin: "0em 0.5em",
-                                width: "max-content",
-                            }}
-                        >
+                        <div className="total">
                             Total Amount:
-                        </div>
-
-                        <div className="vCenter">
-                            <span
-                                className="normal"
-                                style={{
-                                    fontWeight: "bold",
-                                    margin: "0em 0.5em",
-                                }}
-                            >
+                            <span className="normal">
                                 {formValues.overallTotal}
                             </span>
                         </div>
-                    </Col>
-                </>
-            );
-        } else
-            return (
-                <>
-                    <Col className="d-flex justify-content-end">
-                        <div
-                            className="totals gTlabels vCenter"
-                            style={{
-                                fontWeight: "bold",
-                                margin: "0em 0.5em",
-                                width: "auto",
-                            }}
-                        >
-                            Gift Total!:
-                        </div>
-                        <div className="vCenter">
-                            <span className="normal" style={{ padding: "0" }}>
-                                {formValues.giftTotal}
-                            </span>
-                        </div>
-                    </Col>
-                </>
-            );
-    }
-
-    return (
-        <>
-            <div className="row mt-3 border" style={{ padding: "0 1em" }}>
-                {/* <Col className="d-flex">
-                    <div
-                        className="totals gTlabels vCenter"
-                        style={{
-                            fontWeight: "bold",
-                            margin: "0em 0.5em",
-                            width: "auto",
-                        }}
-                    >
-                        Gift Total:
-                    </div>
-                    <div className="vCenter">
-                        <span className="normal" style={{ padding: "0" }}>
-                            {formValues.giftTotal}
-                        </span>
-                    </div>
-                </Col> */}
-
-                {formType === "Cash" || formType === "Check" ? (
-                    <Col className="d-flex">
-                        <div
-                            className="totals gTlabels vCenter"
-                            style={{
-                                fontWeight: "bold",
-                                margin: "0em 0.5em",
-                                width: "auto",
-                            }}
-                        >
-                            Gift Total:
-                        </div>
-                        <div className="vCenter">
-                            <span className="normal" style={{ padding: "0" }}>
-                                {formValues.giftTotal}
-                            </span>
-                        </div>
-                    </Col>
+                    </>
                 ) : (
                     <></>
                 )}
 
-                {NonGiftAmount()}
-
-                <>
-                    {showGIKTotal && (
-                        <Col>
-                            <div className="d-flex">
-                                <div
-                                    className="gTlabels d-flex vCenter"
-                                    style={{
-                                        fontWeight: "bold",
-                                        width: "max-content",
-                                    }}
-                                >
-                                    Gift in Kind Total:
-                                </div>
-                                <div className="vCenter">
-                                    <span
-                                        className="normal gikTotalSpacing"
-                                        style={{
-                                            fontWeight: "bold",
-                                        }}
-                                    >
-                                        {formValues.gikTotal}
-                                    </span>
-                                </div>
-                            </div>
-                        </Col>
-                    )}
-                </>
+                {showGIKTotal ? (
+                    <div className="total">
+                        Gift in Kind:{" "}
+                        <span className="normal">{formValues.gikTotal}</span>
+                    </div>
+                ) : (
+                    <></>
+                )}
             </div>
         </>
     );
